ec9fd82bc850817f98b18ddc76cbed8902cc8e35
Moazzam Abdullah Khan <moazzam2k@yahoo.com>
2016-03-23 22:48:02 +0100

    basic trade done

--- eMAS.js
+++ eMAS.js
@@ -3,29 +3,80 @@ var _frameY = 600;
 var _sX;
 var _sY;
 var _pop;
+var _objArr = [];
+var _sortedArray = [];
+var _honestyMin;
+var _maxConnect;
+var _count;
+var _increment;
 
 	function setup(){
 		createCanvas(_frameX, _frameY);
-		createPopulation(1000, 10, 10);
+		createPopulation(500, 10, 10, 25, 0.8, 10, 0.01);
+		_count = 0;
 	}
 
 	function draw(){
 		background (125);
 
 		updatePopulation();
+		fill(0, 102, 153);
+		textSize(32);
+		text(_count, _frameX - 100, _frameY - 50);
+		_count++;
 	}
 
-	function createPopulation(pop, sX, sY){
+	function createPopulation(pop, sX, sY, defVal, honestyMin, maxConnect, increment){
 		_sX = sX;
 		_sY = sY;
 		_pop = pop;
+		_honestyMin = honestyMin;
+		_maxConnect = maxConnect;
+		_increment = increment;
+
+		for (var i = 0; i < _pop; i++){
+			var tempObj = {
+				objID: i,
+				objVal: defVal,
+				objHonesty: random(_honestyMin, 0.999),
+				objTrade: function(i){
+					if ( this.objHonesty < random() ){
+						// Cheat
+						print ("Object " + this.objID + " cheated " + i);
+						_objArr[i].swindled();
+						this.traded();
+					}
+				},
+				swindled: function(){
+					this.objVal = this.objVal - _increment;
+				},
+				traded: function(){
+					this.objVal = this.objVal + _increment;
+				}
+			};
+
+			_objArr.push(tempObj);
+		}
 	}
 
 	function updatePopulation(){
-		for (var n = 0; n < _pop; n++){
-			rX = random(_frameX/_sX);
-			rY = random(_frameY/_sY);
-			ellipse(rX * _sX, rY * _sY, _sX, _sY);
-		}
+		_sortedArray = _objArr.slice();
+		_sortedArray = _sortedArray.sort(compareObjs);
 
+		for (var i = 0; i < _pop; i++){
+			rX = (_sortedArray[0].objVal - _objArr[i].objVal) / _sortedArray[0].objVal;
+			rY = i/_pop;
+
+			for (var n = 0; n < 1 + floor(rX*_maxConnect); n++) {
+				_objArr[ _sortedArray[i].objID ].objTrade( _sortedArray[ floor( random(i, _pop-1) ) ].objID );
+			}
+			fill(color(255, 0, 0));
+			noStroke();
+			ellipse(rX * _frameX, rY * _frameY, _sX, _sY);
+
+		}
+	}
+
+	function compareObjs(a, b){
+		return b.objVal - a.objVal;
 	}
